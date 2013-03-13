a0f2bba74afb5f7fe80c4254f252ae6c6179624b
Mequrel <mequrel@gmail.com>
2013-03-13 11:32:45 +0100

    Glowna czesc programu wrzucona do main

--- converter.py
+++ converter.py
@@ -1,5 +1,13 @@
 from font import Font, FontLoader
 
+def main():
+	text = getText()
+	font = FontLoader().loadFont('fancyFont/')
+
+	drawer = TextDrawer()
+	drawer.setFont(font)
+	drawer.draw(text)
+
 def getText():
 	return 'Git'
 
@@ -10,9 +18,5 @@ class TextDrawer:
 	def draw(self, text):
 		pass
 
-text = getText()
-font = FontLoader().loadFont('fancyFont/')
-
-drawer = TextDrawer()
-drawer.setFont(font)
-drawer.draw(text)
+if __name__ == '__main__':
+	main()
