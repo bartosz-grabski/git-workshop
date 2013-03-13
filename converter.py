035756a2884a44a3b1895332cf748db6bb62cd9b
Mequrel <mequrel@gmail.com>
2013-03-13 11:32:44 +0100

    mock klasy konwertujacej

--- converter.py
+++ converter.py
@@ -8,6 +8,13 @@ class FontLoader:
 	def loadFont(self, directory):
 		pass
 
+class TextDrawer:
+	def setFont(self, font):
+		pass
+
+	def draw(self, text):
+		pass
+
 t = getText()
 f = FontLoader().loadFont('fancyFont/')
 
