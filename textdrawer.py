0bd5f1b9068e8e6e4304c28281b233549c67e9ef
alexx <alexx1307@gmail.com>
2013-03-22 18:56:26 +0100

    Rysowanie horyzontalne

--- textdrawer.py
+++ textdrawer.py
@@ -3,9 +3,16 @@ class TextDrawer:
 		self.font = font
 
 	def draw(self, text):
+		firstLetter = self.font.letter[text[0].upper()]
+		drawing = splitLetter(firstLetter)
+		
 		for character in text:
-			print self.font.letters[character.upper()]
-
+			letter = splitLetter(self.font.letters[character.upper()]
+			drawing = appendLetter(drawing, letter)
+			
+		for line in drawing:
+			print line
+			
 def splitLetter(letter):
 	return letter.split('\n')
 			
