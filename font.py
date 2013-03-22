fe8b5e14dfc72608145eaac8ec4083f30351169e
Bartek <b.grabski92@gmail.com>
2013-03-22 18:50:06 +0100

    czcionki wydzielone do plikow

--- font.py
+++ font.py
@@ -6,25 +6,7 @@ class Font:
 class FontLoader:
 	def loadFont(self, directory):
 		font = Font("fancy")
-
-		font.letters['G'] = r'''
-  ___ 
- / __)
-( (_ \
- \___/
-'''
-		font.letters['I'] = r'''
-  __  
- (  ) 
-  )(  
- (__) 
-'''
-		font.letters['T'] = r'''
- ____ 
-(_  _)
-  )(  
- (__) 
-'''
+		return font
 
  		return font
 
