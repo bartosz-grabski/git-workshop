1a7ba714be421f72000d85d1adf4e664ec5c304c
alexx <alexx1307@gmail.com>
2013-03-22 19:31:05 +0100

    Revert "Revert "dodanie abs""
    This reverts commit 2f748f77fa0930bf90c188555faa29a2853215bb.

--- converter.py
+++ converter.py
@@ -11,11 +11,9 @@ def main():
 	drawer.draw(text)
 
 def getText():
-	if len(sys.argv) != 1:
-		print "Bad arguments"
-		sys.exit(1)
-		
 	return sys.argv[0]
 
 if __name__ == '__main__':
 	main()
+	
+abs
